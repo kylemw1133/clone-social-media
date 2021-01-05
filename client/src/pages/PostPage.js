190eda9068b10a327c8e8c9f02c0020a6bdaec29
Kyle Williams <kyle@Kyles-iMac.local>
2021-01-04 23:38:41 -0500

    populated dynamic Post page with getPost query data

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -1,9 +1,43 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+import CommentList from "../components/CommentList";
+
+const POSTS_QUERY = gql`
+  query getPost($postId: ID!) {
+    getPost(postId: $postId) {
+      id
+      body
+      createdAt
+      comments {
+        id
+        body
+        createdAt
+        username
+      }
+      likes {
+        username
+        createdAt
+      }
+    }
+  }
+`;
 
 const PostPage = () => {
   const { postId } = useParams();
-  return <div>{postId}</div>;
+  const { loading, error, data } = useQuery(POSTS_QUERY, {
+    variables: { postId },
+  });
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error </p>;
+  return (
+    <div>
+      <p>{data.getPost.createdAt}</p>
+      <p>{data.getPost.body}</p>
+      <CommentList comments={data.getPost.comments} />
+    </div>
+  );
 };
 
 export default PostPage;
